1a20d836cb2b223457bf5780255c23072434b3fc
George Alexandrou <cysystemcy@gmail.com>
2022-04-13 12:55:22 +0300

    secondary commits

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="col-full text-center">
+  <h1>not found </h1>
+  <router-link :to="{name: 'Home'} "> Read some cool threads
+    <button class="btn btn-primary">
+      Go to home
+    </button> </router-link>
+</div>
+  
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
