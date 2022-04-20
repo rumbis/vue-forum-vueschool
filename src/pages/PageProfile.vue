fcf651dea25ab40e4d046c5af141bd6463e7adf0
George Alexandrou <cysystemcy@gmail.com>
2022-04-20 16:43:28 +0300

    Updating the User Profile

--- src/pages/PageProfile.vue
+++ src/pages/PageProfile.vue
@@ -3,6 +3,7 @@
     <div class="flex-grid">
       <div class="col-3 push-top">
         <UserProfileCard :user="user" />
+        <UserProfileCardEditor :user="user" />
 
         <p class="text-xsmall text-faded text-center">
           Member since june 2003, last visited 4 hours ago
@@ -18,32 +19,24 @@
 
       <div class="col-7 push-top">
         <div class="profile-header">
-          <span class="text-lead"> {{ user.username }} recent activity </span>
+          <span class="text-lead"> Joker's recent activity </span>
           <a href="#">See only started threads?</a>
         </div>
-
         <hr />
-
         <PostList :posts="user.posts" />
       </div>
     </div>
   </div>
 </template>
-
 <script>
-import PostList from "@/components/PostList";
-import UserProfileCard from "@/components/UserProfileCard.vue";
-import { mapGetters } from "vuex";
+import PostList from '@/components/PostList'
+import UserProfileCard from '@/components/UserProfileCard'
+import UserProfileCardEditor from '@/components/UserProfileCardEditor'
+import { mapGetters } from 'vuex'
 export default {
-  components: {
-    PostList,
-    UserProfileCard
-  },
-    computed: {
+  components: { PostList, UserProfileCard, UserProfileCardEditor },
+  computed: {
     ...mapGetters({ user: 'authUser' })
   }
 }
 </script>
-
-<style>
-</style>
